3072f521d41fded13d438f750081194f0c5505e1
pep-dev30.ict <pep-dev30.ict@pep.pertamina.com>
2021-02-03 09:16:58 +0700

    add invoice controller and view

--- WGS_PROJ/Controllers/HomeController.cs
+++ WGS_PROJ/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -9,29 +10,53 @@ using WGS_PROJ.Models;
 
 namespace WGS_PROJ.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+    private readonly WGS_PROJContext dbContext = new WGS_PROJContext();
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
     }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult Invoice(int? id)
+    {
+
+      var invoice = dbContext.TrInvoice.Find(id);
+
+      //if (invoice == null) {
+      //  return NotFound();
+      //}
+      
+      ViewBag.sales = new SelectList(dbContext.MsSales, "SalesId", "SalesName");
+      ViewBag.courier = new SelectList(dbContext.MsCourier, "CourierId", "CourierName");
+
+
+      return View(invoice);
+    }
+
+    [HttpPost]
+    public IActionResult Invoice(TrInvoice model)
+    {
+
+      return View();
+    }
+  }
 }
